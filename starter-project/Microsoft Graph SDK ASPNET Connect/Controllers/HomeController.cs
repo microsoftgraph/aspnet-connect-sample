fdb33364dfe7a74f5cb322ee38576fa1c155fdac
Diane Diaz <ddiaz@microsoft.com>
2016-09-15 06:09:09 -0700

    add starter project for walkthrough

---
+++ starter-project/Microsoft Graph SDK ASPNET Connect/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿/* 
+*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+*  See LICENSE in the source repository root for complete license information. 
+*/
+
+using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft_Graph_SDK_ASPNET_Connect.Helpers;
+using Microsoft_Graph_SDK_ASPNET_Connect.Models;
+using Resources;
+
+namespace Microsoft_Graph_SDK_ASPNET_Connect.Controllers
+{
+    public class HomeController : Controller
+    {
+        GraphService graphService = new GraphService();
+
+        public ActionResult Index()
+        {
+            return View("Graph");
+        }
+
+        // Controller actions
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
