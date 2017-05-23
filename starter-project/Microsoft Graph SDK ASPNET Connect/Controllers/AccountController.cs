c7474bf9a3166b2300294019e45554519122fec3
Jim Crowley <jamescro@microsoft.com>
2017-05-23 16:20:43 -0700

    Updating starter project for MSAL

--- starter-project/Microsoft Graph SDK ASPNET Connect/Controllers/AccountController.cs
+++ starter-project/Microsoft Graph SDK ASPNET Connect/Controllers/AccountController.cs
@@ -16,5 +16,35 @@ namespace Microsoft_Graph_SDK_ASPNET_Connect.Controllers
 {
     public class AccountController : Controller
     {
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                // Signal OWIN to send an authorization request to Azure.
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                  new AuthenticationProperties { RedirectUri = "/" },
+                  OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        // Here we just clear the token cache, sign out the GraphServiceClient, and end the session with the web app.  
+        public void SignOut()
+        {
+            if (Request.IsAuthenticated)
+            {
+                // Get the user's token cache and clear it.
+                string userObjectId = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                SessionTokenCache tokenCache = new SessionTokenCache(userObjectId, HttpContext);
+                HttpContext.GetOwinContext().Authentication.SignOut(OpenIdConnectAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType);
+            }
+
+            SDKHelper.SignOutClient();
+
+            // Send an OpenID Connect sign-out request. 
+            HttpContext.GetOwinContext().Authentication.SignOut(
+              CookieAuthenticationDefaults.AuthenticationType);
+            Response.Redirect("/");
+        }
     }
 }
